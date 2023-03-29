882ead67ef76b485e1f447c8368102f4dcf6803c
Jaskaran Singh Dhillon <dhillonjaskaran4486@gmail.com>
2023-03-30 00:31:41 +0530

    made workout section responsive

--- JAVASCRIPT/WorkoutAndExerciseScript.js
+++ JAVASCRIPT/WorkoutAndExerciseScript.js
@@ -1,4 +1,13 @@
 "use strict";
+
+const linksNavBar = document.querySelector(".links");
+const menuButton = document.querySelector("nav .menuButton");
+
+// To show ot hide nav bar
+menuButton.addEventListener("click", function () {
+  linksNavBar.classList.toggle("hide");
+});
+
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
