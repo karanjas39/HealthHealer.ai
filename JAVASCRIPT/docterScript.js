f6153ecb3dd6c3257cc5b670344726e5e596b999
Jaskaran Singh Dhillon <dhillonjaskaran4486@gmail.com>
2023-03-29 23:23:44 +0530

    Updated index.html and make docter section responsive

--- JAVASCRIPT/docterScript.js
+++ JAVASCRIPT/docterScript.js
@@ -1,4 +1,13 @@
 "use strict";
+
+const linksNavBar = document.querySelector(".links");
+const menuButton = document.querySelector("nav .menuButton");
+
+// To show ot hide nav bar
+menuButton.addEventListener("click", function () {
+  linksNavBar.classList.toggle("hide");
+});
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
